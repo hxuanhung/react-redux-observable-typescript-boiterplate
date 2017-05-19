b6d481d6bdbdd25a4da479c02bd394cfad83fecc
hxhung <hung.xuan@kellydeli.com>
2017-05-19 18:06:09 +0200

    chore(): use port 4000

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,13 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, Switch } from 'react-router';
-import { createBrowserHistory } from 'history';
-import { configureStore } from './store';
-import { App } from './containers/App';
-
-const store = configureStore();
-const history = createBrowserHistory();
+import App from './App';
+import store from './store/configureStore';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root')
 );
